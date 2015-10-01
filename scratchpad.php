7485c6bed69313ca25643dbeb34480d03454ff78
Patrick <patrickmaynard.bs@gmail.com>
2015-10-01 16:55:26 -0400

    Update scratchpad.php

--- scratchpad.php
+++ scratchpad.php
@@ -4,31 +4,54 @@ ini_set("display_errors","1");
 error_reporting(E_ALL);
 $noticechecker .= " ";
 
+//This project is being built for the purpose of building and iterating through a local grid.
+//The goal is to establish the grid point that has the highest density of an item within a defined radius.
 //A lot of this will probably eventually be broken out into other places, of course.
 
-class BaltLocTool{
-
- public function getRoughPairDistance($lat1,$lon1,$lat2,$lon2){
+class BaltGeoTools{
+ public function __construct(){
         //Remember: This only works on areas in Baltimore's plane. 
         //Despite what Singing Science Records might tell us, the phrase "the earth is a ball" is not entirely accurate.
         //It's more of a round-ish potato. So lat and lon values can "stretch"
         //Lon: -76.673608 to -76.582241 is exactly five miles along North Avenue, sez Bing.
-        //      So abs(76.582241 - 76.673608)/5 = 1 mile worth of radius = lonmile
-        $lonmile = abs(76.582241 - 76.673608)/5;
+        //      So abs(76.582241 - 76.673608)/5 = 1 mile worth of radius = this->lonmile
+        $this->lonmile = abs(76.582241 - 76.673608)/5;
         //Lat (via four miles on Light Street): 
-        $latmile = abs(39.327998 - 39.271404)/4;
-        //Now remember from 6th grade: a^2 + b^2 = c^2.
-        $latdistance = abs($lat1 - $lat2)/$latmile;
-        $londistance = abs($lon1 - $lon2)/$lonmile;
+        $this->latmile = abs(39.327998 - 39.271404)/4;
+ }
+ public function getRoughPairDistance($lat1,$lon1,$lat2,$lon2){
+        //This function uses basic pythagorean scoodleedoop.
+        //It spits out a back-of-napkin distance between two points.
+        //Precision is somewhat limited, but ok for our purpose. See disclaimer above. 
+        $latdistance = abs($lat1 - $lat2)/$this->latmile;
+        $londistance = abs($lon1 - $lon2)/$this->lonmile;
         return($distance);
  }
-
+ public function createGrid($feetperblock = 250, $startlat = 39.377417, $startlon = -76.715646, $endlat = '39.183335', $endlon = '-76.504953'){
+        print("\n This is a stub of a method that will create and return a two-dimensional array of roughly-block-sized points from across a region. \n");
+        $latinc = ($this->latmile/5280) * $feetperblock;
+        $loninc = ($this->lonmile/5280) * $feetperblock;
+        $coords = array();
+        for($lat = $startlat; $lat > $endlat; $lat -= $latinc){
+                for($lon = $startlon; $lon < $endlon; $lon += $loninc){
+                        $coords[] = array('lat'=>$lat,'lon'=>$lon);
+                }
+        }
+ }
+ public function getItems(){
+        print("\n This is a stub of a method that will read a csv of items into an array. \n");
+ }
+ public function getCounts(){
+        print("\n This is a stub of a method that will iterate through each grid point, calculating the number of items within a block. \n");
+ }
 }
 
 
 //Now some tests ... 
 
-$blt = new BaltLocTool;
+$blt = new BaltGeoTools;
+
+$blt->createGrid();
 
 /*
 //39.309919, -76.642486 to 39.299849, -76.630266 should be almost exactly a mile, according to Google.
